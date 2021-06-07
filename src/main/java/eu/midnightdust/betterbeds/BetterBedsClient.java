a847d0d2892a40ce4a4961c98ceb7cca3f1c9cdc
Motschen <motschenfm@gmail.com>
2021-06-07 18:28:35 +0200

    Update to 1.17-pre1

--- src/main/java/eu/midnightdust/betterbeds/BetterBedsClient.java
+++ src/main/java/eu/midnightdust/betterbeds/BetterBedsClient.java
@@ -2,16 +2,16 @@ package eu.midnightdust.betterbeds;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
+import net.fabricmc.fabric.api.resource.ResourcePackActivationType;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.util.Identifier;
 
 public class BetterBedsClient implements ClientModInitializer {
 
     public void onInitializeClient() {
-
         FabricLoader.getInstance().getModContainer("betterbeds").ifPresent(modContainer -> {
-            ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("betterbeds:fancybeds"), "resourcepacks/fancybeds", modContainer, false);
-            ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("betterbeds:connectedbeds"), "resourcepacks/connectedbeds", modContainer, false);
+            ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("betterbeds:fancybeds"),  modContainer, ResourcePackActivationType.NORMAL);
+            ResourceManagerHelper.registerBuiltinResourcePack(new Identifier("betterbeds:connectedbeds"),  modContainer, ResourcePackActivationType.NORMAL);
         });
     }
 }
--- src/main/java/eu/midnightdust/betterbeds/mixin/MixinBlockEntityRendererDispatcher.java
+++ src/main/java/eu/midnightdust/betterbeds/mixin/MixinBlockEntityRendererDispatcher.java
@@ -1,17 +1,11 @@
 package eu.midnightdust.betterbeds.mixin;
 
-import com.google.common.collect.Maps;
-import net.minecraft.block.BedBlock;
-import net.minecraft.block.BlockRenderType;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalFacingBlock;
+import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.block.entity.BlockEntityType;
-import net.minecraft.client.render.block.entity.BlockEntityRenderDispatcher;
-import net.minecraft.client.render.block.entity.BlockEntityRenderer;
-import net.minecraft.util.math.Direction;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactories;
+import net.minecraft.client.render.block.entity.BlockEntityRendererFactory;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -19,12 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Map;
 
-@Mixin(BlockEntityRenderDispatcher.class)
+@Mixin(BlockEntityRendererFactories.class)
 public abstract class MixinBlockEntityRendererDispatcher {
-    @Shadow @Final private Map<BlockEntityType<?>, BlockEntityRenderer<?>> renderers;
 
-    @Inject(method = "<init>()V", at = @At("TAIL"))
-    private void init(CallbackInfo info) {
-        renderers.remove(BlockEntityType.BED);
+    @Shadow @Final private static Map<BlockEntityType<?>, BlockEntityRendererFactory<?>> FACTORIES;
+
+    @Inject(method = "register", at = @At("TAIL"))
+    private static <T extends BlockEntity>  void bb$onRegister(BlockEntityType<? extends T> type, BlockEntityRendererFactory<T> factory, CallbackInfo ci) {
+        FACTORIES.remove(BlockEntityType.BED);
     }
 }
