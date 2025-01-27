0a9f7ea0a1cdb0b1924492da4489414cd627fb49
Martin Prokoph <motschen@midnightdust.eu>
2025-01-27 14:31:46 +0100

    port: MC 1.21.4

--- common/src/main/java/eu/midnightdust/betterbeds/mixin/MixinBedBlock.java
+++ common/src/main/java/eu/midnightdust/betterbeds/mixin/MixinBedBlock.java
@@ -1,11 +1,10 @@
 package eu.midnightdust.betterbeds.mixin;
 
-import net.minecraft.block.*;
+import net.minecraft.block.BedBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.HorizontalFacingBlock;
 import net.minecraft.util.math.Direction;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(value = BedBlock.class, priority = 2000)
 public abstract class MixinBedBlock extends HorizontalFacingBlock {
@@ -14,13 +13,7 @@ public abstract class MixinBedBlock extends HorizontalFacingBlock {
         super(settings);
     }
 
-    @Inject(at = @At("RETURN"), method = "getRenderType", cancellable = true)
-    private void getRenderType(BlockState state, CallbackInfoReturnable<BlockRenderType> cir) {
-        cir.setReturnValue(BlockRenderType.MODEL);
-    }
-
     @Override
-    @SuppressWarnings("deprecation")
     public boolean isSideInvisible(BlockState state, BlockState neighborState, Direction offset) {
         return neighborState.getBlock() instanceof BedBlock;
     }
